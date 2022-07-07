329d34f3afdf6451ae54d000689cbeb9133d2a75
石原枝林下 <87614796+Ishiharaerika@users.noreply.github.com>
2022-07-07 21:00:33 +0900

    Update AfterPart.sh

--- AfterPart.sh
+++ AfterPart.sh
@@ -12,10 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-# git clone
-git clone https://github.com/vernesong/OpenClash
-git clone https://github.com/jerrykuku/luci-app-argon-config
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone https://github.com/jerrykuku/luci-theme-argon
-git clone https://github.com/lisaac/luci-app-dockerman
-git clone https://github.com/f8q8/luci-app-filetransfer
